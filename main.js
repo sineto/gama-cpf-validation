9e00b6d085a1ffa29040e325c82e684fc719da91
Sinésio Neto <sn.logbackup@gmail.com>
2020-05-03 01:53:19 -0300

    Update repository

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function verifierValidation(cpf, base) {
-  var sum = 0;
-  var mod = 0;
+  let sum = 0;
+  let mod = 0;
   for (let i = 1; i <= base; i++) {
     sum += parseInt(cpf.charAt(i-1)) * ((base + 2) - i);
     mod = (sum * 10) % 11
@@ -18,8 +18,8 @@ function verifierValidation(cpf, base) {
 }
 
 function cpfValid(cpf) {
-  var firstVerifier = verifierValidation(cpf, 9);
-  var secondVerifier = verifierValidation(cpf, 10);
+  let firstVerifier = verifierValidation(cpf, 9);
+  let secondVerifier = verifierValidation(cpf, 10);
 
   if (cpf.length != 11) {
     return false;
@@ -31,31 +31,32 @@ function cpfValid(cpf) {
 }
 
 function cpfMask() {
-  var mask = '###.###.###-##';
-  var input = inputEl.value.length;
-  var out = mask.substring(0, 1);
-  var text = mask.substring(input);
+  let mask = '###.###.###-##';
+  let input = inputEl.value.length;
+  let out = mask.substring(0, 1);
+  let text = mask.substring(input);
 
   if (text.substring(0, 1) != out) {
     inputEl.value += text.substring(0, 1);
   }
 }
 
-var inputEl = document.getElementById('cpf');
 var submitEl = document.getElementById('action');
-var alertDiv = document.getElementsByClassName('clean-div');
+var inputEl = document.getElementById('cpf');
 inputEl.onkeypress = cpfMask;
 
 submitEl.onclick = () => {
-  var cpf = inputEl.value.replace(/\.|-/g, '');
-  var isCpfValid = cpfValid(cpf)
+	let cpf = inputEl.value.replace(/\.|-/g, '');
+	let isCpfValid = cpfValid(cpf)
 
-  document.getElementById('success').style.display = 'none';
-  document.getElementById('error').style.display = 'none';
+	document.getElementById('success').style.display = 'none';
+	document.getElementById('error').style.display = 'none';
+
   inputEl.value = '';
 
   if (isCpfValid) {
     success.style.display = 'flex';
+
   } else {
     error.style.display = 'flex';
   }
